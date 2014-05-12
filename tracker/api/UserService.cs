9c71313b96635fd7bb91249166f4e6929ab19381
gchudublin <chu_greg@yahoo.com>
2014-05-11 22:44:15 -0400

    test

---
+++ tracker/api/UserService.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tracker.api
+{
+    public class UserService : Service
+    {
+        public object Post(AddUser request)
+        {
+            return new AddUserResponse(UserId = 255);
+        }
+    }
+    class AddUserResponse
+    {
+        public long UserId { get; set; }
+    }
+
+    [Route ("/users","POST")]
+    class AddUser
+    {
+        public string Name { get; set; }
+        public int Goal { get; set; }
+    }
+    
+}
